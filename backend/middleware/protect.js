5375cbfab02cad20d741b36a61534c6f8fc8869f
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-15 16:11:05 +0900

    update protect.js for decoded.id

--- backend/middleware/protect.js
+++ backend/middleware/protect.js
@@ -15,12 +15,12 @@ exports.protect = async (req, res, next) => {
       // 토큰 검증, 페이로드 추출
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      if (!decoded || !decoded.id.id) {
+      if (!decoded || !decoded.id) {
         return res.status(401).send({ message: `Invalid token` });
       }
 
       // 페이로드에서 사용자 정보를 가져와 다음 미들웨어나 라우터에서 사용
-      req.user = await UserModel.findUserById(decoded.id.id);
+      req.user = await UserModel.findUserById(decoded.id);
       if (!req.user) {
         return res.status(401).send({ message: `User not found in database` });
       }
