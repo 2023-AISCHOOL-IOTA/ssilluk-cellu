7715dc54d4a6fa0e839ca26aa4cc21eb9f8150e1
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-16 10:20:01 +0900

    Feat: add stress features

--- backend/controllers/sensorController.js
+++ backend/controllers/sensorController.js
@@ -1,22 +1,33 @@
 // NOTE: 센서 데이터를 받아 예측 모델 서버에 전송, 결과를 데이터베이스에 저장
-
-/**
- * @param res - 응답 객체
- * @param req - 요청 객체
- * @param next - 다음 미들웨어로 넘기는 함수
- */
-
 const axios = require("axios");
+const { encrypt } = require("../utils/encryptionUtils");
 const sensorModel = require("../models/sensorModel");
 const notificationService = require("../services/notificationService");
 
 const PREDICTION_MODEL_SERVER_URL = `http://${process.env.PREDICTION_SERVER_HOSTNAME}:${process.env.PREDICTION_SERVER_PORT}/${process.env.PREDICTION_SERVER_PATH}`;
+
+// 스트레스 수준 판별 함수
+const getStressLevel = (frequencyRatio) => {
+  if (
+    (frequencyRatio >= 0.3 && frequencyRatio <= 0.4) ||
+    (frequencyRatio >= 0.6 && frequencyRatio <= 0.7)
+  ) {
+    return "initial_stress";
+  } else if (frequencyRatio > 0.4 && frequencyRatio < 0.6) {
+    return "normal";
+  } else {
+    return "high_stress";
+  }
+};
 
 const sensorController = {
   processSensorData: async (req, res, next) => {
     try {
       const userEmail = req.user.email; // 인증된 사용자의 ID
       const sensorData = req.body; // 센서 데이터
+
+      // 스트레스 수준 계산
+      const stressLevel = getStressLevel(sensorData.fr);
 
       // 예측 모델 서버로 데이터 전송 및 결과 받기
       const { data } = await axios.post(
@@ -25,8 +36,15 @@ const sensorController = {
       );
       const predictionResult = data.prediction;
 
+      // 데이터 암호화
+      const encryptedSensorData = encrypt(JSON.stringify(sensorData));
+
       // 데이터베이스에 센서 데이터와 예측 결과 저장
-      await sensorModel.saveData(userEmail, sensorData, predictionResult);
+      await sensorModel.saveData(
+        userEmail,
+        encryptedSensorData,
+        predictionResult
+      );
 
       // 예측 결과가 54 이하 또는 공복시 300 이상인 경우 SMS 알림 전송
       if (
@@ -56,9 +74,8 @@ const sensorController = {
           }
         }
       }
-
       // 클라이언트에 성공 응답 전송
-      res.status(200).json(predictionResult);
+      res.status(200).json({ predictionResult, stressLevel });
     } catch (error) {
       // 에러 처리
       next(error);
