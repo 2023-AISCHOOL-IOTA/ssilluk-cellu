f83c0d9885fa40e3336e03d3c7764f87ce65fa79
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-20 16:01:11 +0900

    Feat: change design patter to bloc for splash screen

---
+++ cellu/lib/widgets/splash_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import '../bloc/server_connection_bloc.dart';
+import '../screens/login_screen.dart';
+import '../services/logger_service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/services.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<ServerConnectionBloc>(context).add(CheckConnection());
+  }
+
+  void _navigateToLogin() {
+    if (!mounted) return;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
+  void _showError() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('연결 오류'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('서버에 연결할 수 없습니다.'),
+                Text('네트워크 상태를 확인하거나 나중에 다시 시도해주세요.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('다시 시도'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                BlocProvider.of<ServerConnectionBloc>(context)
+                    .add(CheckConnection());
+              },
+            ),
+            TextButton(
+              child: const Text('종료'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                SystemNavigator.pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ServerConnectionBloc, ServerConnectionState>(
+      listener: (context, state) {
+        if (state == ServerConnectionState.connected) {
+          _navigateToLogin();
+        } else if (state == ServerConnectionState.error) {
+          _showError();
+        }
+      },
+      builder: (context, state) {
+        if (state == ServerConnectionState.initial) {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(child: SvgPicture.asset('assets/logo.svg')),
+          );
+        }
+      },
+    );
+  }
+}
