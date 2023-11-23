95e60dfe89cc9bddc425354f7ad8b1ac19707b48
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-23 12:36:24 +0900

    Fix: add error log

--- cellu/lib/repository/server_connection_repository.dart
+++ cellu/lib/repository/server_connection_repository.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../../services/logger_service.dart';
 
 class ServerConnectionRepository {
   Future<bool> checkConnection() async {
@@ -7,6 +8,16 @@ class ServerConnectionRepository {
       final response = await http.get(Uri.parse(dotenv.env['BACKEND_URL']!));
       return response.statusCode == 200;
     } catch (e) {
+      LoggerService.error('Server Connection Failed', error: e);
+
+      if (e is http.ClientException) {
+        // HTTP 클라이언트 예외의 경우
+        LoggerService.error('HTTP Client Exception', error: e.message);
+      } else {
+        // 기타 예외의 경우
+        LoggerService.error('Unknown Exception', error: e.toString());
+      }
+
       throw Exception('Server Connection Failed: $e');
     }
   }
