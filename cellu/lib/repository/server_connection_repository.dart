74a0673853b4c51ba24b48bd7802912ef3738d73
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-23 15:08:02 +0900

    Fix: edit server connection

--- cellu/lib/repository/server_connection_repository.dart
+++ cellu/lib/repository/server_connection_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../../services/logger_service.dart';
@@ -5,7 +6,11 @@ import '../../services/logger_service.dart';
 class ServerConnectionRepository {
   Future<bool> checkConnection() async {
     try {
+      // TODO: DELETE
+      print('response start here');
       final response = await http.get(Uri.parse(dotenv.env['BACKEND_URL']!));
+      // TODO: DELETE
+      print('response:  $response');
       return response.statusCode == 200;
     } catch (e) {
       LoggerService.error('Server Connection Failed', error: e);
