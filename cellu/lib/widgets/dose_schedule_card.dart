34f94cf6cefc6d38bc2c1d7a0c1dcc913a1ddb14
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-24 20:50:26 +0900

    Fix: edit color and styles

--- cellu/lib/widgets/dose_schedule_card.dart
+++ cellu/lib/widgets/dose_schedule_card.dart
@@ -5,20 +5,56 @@ import 'package:cellu/models/dose_schedule_model.dart';
 class DoseScheduleCard extends StatelessWidget {
   final List<DoseScheduleItem> scheduleItems;
 
-  const DoseScheduleCard({required this.scheduleItems});
+  const DoseScheduleCard({Key? key, required this.scheduleItems})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      elevation: 4.0,
-      margin: const EdgeInsets.all(12.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
+    List<Widget> itemWidgets =
+        scheduleItems.map((item) => item.build(context)).toList();
+
+    return Container(
+      margin: EdgeInsets.all(AppDimensions.pagePaddingHorizontal),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.cardCornerRadius),
+        ),
+        color: AppColors.dosePrimaryColor,
         child: Column(
-          children: scheduleItems.map((item) => item.build(context)).toList(),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: AppDimensions.doseItemPadding,
+                horizontal: AppDimensions.pagePaddingHorizontal,
+              ),
+              decoration: BoxDecoration(
+                color: AppColors.dosePrimaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(AppDimensions.cardCornerRadius),
+                  topRight: Radius.circular(AppDimensions.cardCornerRadius),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '약 복용',
+                    style: AppStyles.doseItemTitleStyle
+                        .copyWith(color: AppColors.white),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add, color: AppColors.white),
+                    // '추가' 아이콘 버튼
+                    onPressed: () {
+                      // TODO: '추가' 버튼이 클릭되었을 때의 동작을 정의합니다.
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ...itemWidgets,
+          ],
         ),
       ),
     );
