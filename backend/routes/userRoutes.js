6efcc60265c355370d2dfa063fab6724aef4b6ff
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-27 09:52:44 +0900

    Fix: update routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,25 @@ const { protect } = require("../middleware/protect");
 
 const router = express.Router();
 
+// TODO: DELETE
+// 미들웨어 함수를 생성하여 요청과 응답을 로그에 출력
+function logRequestAndResponse(req, res, next) {
+  console.log("Request:", req.method, req.url);
+  console.log("Request Headers:", req.headers);
+  console.log("Request Body:", req.body);
+
+  // 응답을 가로채서 응답 내용을 출력
+  const originalSend = res.send;
+  res.send = function (body) {
+    console.log("Response:", res.statusCode);
+    console.log("Response Body:", body);
+    originalSend.call(this, body);
+  };
+
+  next();
+}
+router.use(logRequestAndResponse);
+
 router.post("/signup", userController.signUp);
 router.post("/signin", userController.signIn);
 // 로그아웃은 클라이언트에서 처리
