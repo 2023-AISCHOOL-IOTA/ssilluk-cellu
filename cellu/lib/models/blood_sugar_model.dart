6b13c4e15d623c1148b71e58850850037efdd14c
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-28 20:29:03 +0900

    Fix: edit date filtering

--- cellu/lib/models/blood_sugar_model.dart
+++ cellu/lib/models/blood_sugar_model.dart
@@ -11,7 +11,7 @@ class BloodSugarModel {
   final String token; // 사용자의 JWT 토큰
   BloodSugarModel(this.token);
 
-  Future<Map<int, List<int>>> fetchBloodSugarData(String userId) async {
+  Future<Map<DateTime, List<int>>> fetchBloodSugarData(String userId) async {
     final url = Uri.parse('${dotenv.env['BACKEND_URL']}/sensor');
     // POST 요청 전에 로그 추가
     LoggerService.info('Sending a GET request to fetch blood sugar data');
@@ -22,37 +22,16 @@ class BloodSugarModel {
 
     if (response.statusCode == 200) {
       var data = json.decode(response.body);
-      Map<int, List<int>> result = {};
-
-      // 가장 최근의 데이터를 추적하기 위한 변수를 nullable 타입으로 선언
-      DateTime? mostRecentTime;
-      int? mostRecentValue;
+      Map<DateTime, List<int>> result = {};
 
       // 데이터 파싱 및 변환 로직
       for (var item in data) {
         int? bloodSugar = item['bloodsugar'];
         if (bloodSugar != null) {
-          DateTime dateTime = DateTime.parse(item['created_at']);
-          int hour = int.parse(DateFormat('H').format(dateTime));
-          int hourGroup = (hour / 3).floor() * 3;
+          DateTime utcDateTime = DateTime.parse(item['created_at']);
+          DateTime localDateTime = utcDateTime.toLocal();
 
-          result.putIfAbsent(hourGroup, () => []).add(bloodSugar);
-
-          // 가장 최근의 데이터 업데이트
-          if (mostRecentTime == null || dateTime.isAfter(mostRecentTime)) {
-            mostRecentTime = dateTime;
-            mostRecentValue = bloodSugar;
-          }
-        }
-      }
-      // 가장 최근의 데이터 추가 로직
-      if (mostRecentValue != null && mostRecentTime != null) {
-        int mostRecentHourGroup = (mostRecentTime!.hour / 3).floor() * 3;
-        if (!result.containsKey(mostRecentHourGroup) ||
-            !result[mostRecentHourGroup]!.contains(mostRecentValue)) {
-          result
-              .putIfAbsent(mostRecentHourGroup, () => [])
-              .add(mostRecentValue);
+          result.putIfAbsent(localDateTime, () => []).add(bloodSugar);
         }
       }
       return result;
