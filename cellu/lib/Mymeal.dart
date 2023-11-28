64f2d233ca8346f9732163d19cce2ef4dd291d35
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-28 20:29:56 +0900

    Fix: edit conflict

--- cellu/lib/Mymeal.dart
+++ cellu/lib/Mymeal.dart
@@ -1,4 +1,4 @@
-import 'package:ssilluk-cellu/MealCreate.dart';
+import 'package:cellu/MealCreate.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
