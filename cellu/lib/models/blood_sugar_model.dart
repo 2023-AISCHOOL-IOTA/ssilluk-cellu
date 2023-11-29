c3a6e90ab7753ddfea64aec442f93893437c8838
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-29 12:12:43 +0900

    Fix: edit date filtering

--- cellu/lib/models/blood_sugar_model.dart
+++ cellu/lib/models/blood_sugar_model.dart
@@ -1,44 +1,32 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import 'package:cellu/styles.dart';
-import 'package:cellu/utils/user_token_manager.dart';
+import 'package:cellu/repository/server_connection_repository.dart';
 import 'package:cellu/services/logger_service.dart';
-import 'package:intl/intl.dart';
 
 class BloodSugarModel {
   final String token; // 사용자의 JWT 토큰
   BloodSugarModel(this.token);
 
   Future<Map<DateTime, List<int>>> fetchBloodSugarData(String userId) async {
-    final url = Uri.parse('${dotenv.env['BACKEND_URL']}/sensor');
-    // POST 요청 전에 로그 추가
-    LoggerService.info('Sending a GET request to fetch blood sugar data');
-    final response =
-        await http.get(url, headers: {'Authorization': 'Bearer $token'});
-    // POST 요청 후에 로그 추가
-    LoggerService.info('Received response from server: ${response.body}');
+    final url = '${dotenv.env['BACKEND_URL']}/sensor';
+    final headers = {'Authorization': 'Bearer $token'};
 
-    if (response.statusCode == 200) {
-      var data = json.decode(response.body);
-      Map<DateTime, List<int>> result = {};
+    // ServerConnectionRepository를 사용하여 데이터 가져오기
+    var data =
+        await ServerConnectionRepository().fetchData(url, headers: headers);
 
-      // 데이터 파싱 및 변환 로직
-      for (var item in data) {
-        int? bloodSugar = item['bloodsugar'];
-        if (bloodSugar != null) {
-          DateTime utcDateTime = DateTime.parse(item['created_at']);
-          DateTime localDateTime = utcDateTime.toLocal();
-
-          result.putIfAbsent(localDateTime, () => []).add(bloodSugar);
-        }
+    Map<DateTime, List<int>> result = {};
+    // 데이터 파싱 및 변환 로직
+    for (var item in data) {
+      int? bloodSugar = item['bloodsugar'];
+      if (bloodSugar != null) {
+        DateTime localDateTime = DateTime.parse(item['created_at']).toLocal();
+        result.putIfAbsent(localDateTime, () => []).add(bloodSugar);
       }
-      return result;
-    } else {
-      // 오류 처리
-      throw Exception('Failed to load blood sugar data');
     }
+    return result;
   }
 
   Color getSugarLevelColor(int level, bool isMax) {
--- cellu/lib/models/dose_schedule_model.dart
+++ cellu/lib/models/dose_schedule_model.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:cellu/styles.dart';
-import 'package:cellu/utils/user_token_manager.dart';
+import 'package:cellu/repository/server_connection_repository.dart';
 import 'package:cellu/services/logger_service.dart';
 
 // 약 복용 스케줄 아이템 모델
@@ -100,39 +99,19 @@ class DoseScheduleItem {
 // 약 복용 스케줄 데이터를 가져오는 모델
 class DoseScheduleItemModel {
   Future<List<DoseScheduleItem>> fetchDoseScheduleData(String token) async {
+    final serverConnectionRepository = ServerConnectionRepository();
+    final url = '${dotenv.env['BACKEND_URL']}/dose';
+    final headers = {'Authorization': 'Bearer $token'};
+
     try {
-      // 토큰 검증
-      if (token.isEmpty) {
-        throw Exception('Token is not set.');
-      }
-
-      // POST 요청 전에 로그 추가
-      LoggerService.info('Sending a GET request to fetch dose schedule data');
-      final url = Uri.parse('${dotenv.env['BACKEND_URL']}/dose');
-      final response =
-          await http.get(url, headers: {'Authorization': 'Bearer $token'});
-      // POST 요청 후에 로그 추가
-      LoggerService.info('Received response from server: ${response.body}');
-
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        return data.map((item) => DoseScheduleItem.fromJson(item)).toList();
-      } else {
-        throw Exception('Failed to load dose schedule data');
-      }
+      final data =
+          await serverConnectionRepository.fetchData(url, headers: headers);
+      return data
+          .map<DoseScheduleItem>((item) => DoseScheduleItem.fromJson(item))
+          .toList();
     } catch (e) {
       LoggerService.error('Error fetching dose schedule data: $e');
       throw Exception('Failed to load dose schedule data');
     }
-  }
-
-  List<DoseScheduleItem> filterDoseScheduleByDate(
-      List<DoseScheduleItem> scheduleItems, DateTime filterDate) {
-    return scheduleItems.where((item) {
-      final itemDate = DateTime.parse(item.doseTime);
-      return itemDate.year == filterDate.year &&
-          itemDate.month == filterDate.month &&
-          itemDate.day == filterDate.day;
-    }).toList();
   }
 }
