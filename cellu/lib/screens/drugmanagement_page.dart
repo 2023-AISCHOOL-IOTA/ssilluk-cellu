fac75e469536f2774b5fde26c6459f9643172bb3
leekh8 <125537212+leekh8@users.noreply.github.com>
2023-11-29 12:13:27 +0900

    Fix: edit input dose page

--- cellu/lib/screens/drugmanagement_page.dart
+++ cellu/lib/screens/drugmanagement_page.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 import 'package:intl/intl.dart';
 import 'package:cellu/styles.dart';
-import 'package:cellu/screens/Home_screen.dart';
+import 'package:cellu/screens/home_screen.dart';
 import 'package:cellu/utils/user_token_manager.dart';
 import 'package:cellu/widgets/lowerbar.dart';
+import 'package:cellu/models/dose_schedule_model.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 
 class DrugManagement extends StatelessWidget {
   const DrugManagement({Key? key});
@@ -30,9 +31,73 @@ class _DrugInputPageState extends State<DrugInputPage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController doseController = TextEditingController();
   DateTime selectedDateTime = DateTime.now(); // 선택된 날짜와 시간을 저장할 변수
-
-  // 식사 전/후 여부를 저장할 변수
   bool isBeforeMeal = true;
+
+  final FocusNode _typeFocus = FocusNode();
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _doseFocus = FocusNode();
+
+  void _saveDrugData() {
+    _sendDrugDataToServer(
+      typeController.text,
+      nameController.text,
+      doseController.text,
+      selectedDateTime,
+      isBeforeMeal,
+      context,
+    );
+  }
+
+  void _sendDrugDataToServer(
+    String type,
+    String name,
+    String dose,
+    DateTime dateTime,
+    bool isBeforeMeal,
+    BuildContext context,
+  ) async {
+    final token = UserTokenManager.getToken();
+    if (token != null) {
+      final utcDateTime = dateTime.toUtc();
+      final formattedDateTime =
+          DateFormat('yyyy-MM-dd HH:mm').format(utcDateTime);
+
+      final data = {
+        'medicine_type': type,
+        'dose_medicine': name,
+        'dose_amount': dose,
+        'dose_time': formattedDateTime,
+        'meal_yn': isBeforeMeal ? 'N' : 'Y',
+      };
+
+      final url = Uri.parse('${dotenv.env['BACKEND_URL']}/dose');
+
+      try {
+        final response = await http.post(
+          url,
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode(data),
+        );
+
+        if (response.statusCode == 201) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('약물 정보가 성공적으로 전송되었습니다.')));
+        } else {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('약물 정보 전송에 실패했습니다.')));
+        }
+      } catch (error) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('약물 저장 오류: $error')));
+      }
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('인증 토큰이 없습니다.')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,34 +128,31 @@ class _DrugInputPageState extends State<DrugInputPage> {
                   Container(
                     width: 150,
                     child: GestureDetector(
-                      onTap: () {
-                        // 날짜와 시간 선택 다이얼로그 열기
-                        showDatePicker(
+                      onTap: () async {
+                        final pickedDate = await showDatePicker(
                           context: context,
                           initialDate: selectedDateTime,
                           firstDate: DateTime(2001),
                           lastDate: DateTime(2101),
-                        ).then((pickedDate) {
-                          if (pickedDate != null) {
-                            showTimePicker(
-                              context: context,
-                              initialTime:
-                                  TimeOfDay.fromDateTime(selectedDateTime),
-                            ).then((pickedTime) {
-                              if (pickedTime != null) {
-                                setState(() {
-                                  selectedDateTime = DateTime(
-                                    pickedDate.year,
-                                    pickedDate.month,
-                                    pickedDate.day,
-                                    pickedTime.hour,
-                                    pickedTime.minute,
-                                  );
-                                });
-                              }
+                        );
+                        if (pickedDate != null) {
+                          final pickedTime = await showTimePicker(
+                            context: context,
+                            initialTime:
+                                TimeOfDay.fromDateTime(selectedDateTime),
+                          );
+                          if (pickedTime != null) {
+                            setState(() {
+                              selectedDateTime = DateTime(
+                                pickedDate.year,
+                                pickedDate.month,
+                                pickedDate.day,
+                                pickedTime.hour,
+                                pickedTime.minute,
+                              );
                             });
                           }
-                        });
+                        }
                       },
                       child: Container(
                         padding: EdgeInsets.all(10),
@@ -100,14 +162,10 @@ class _DrugInputPageState extends State<DrugInputPage> {
                           borderRadius: BorderRadius.circular(18),
                           color: AppColors.lightGreyOpacity20,
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              '날짜, 시간 선택',
-                              style: AppStyles.doseItemSubtitleStyle,
-                            ),
-                          ],
+                        child: Text(
+                          DateFormat('yyyy-MM-dd HH:mm')
+                              .format(selectedDateTime),
+                          style: AppStyles.doseItemSubtitleStyle,
                         ),
                       ),
                     ),
@@ -139,32 +197,36 @@ class _DrugInputPageState extends State<DrugInputPage> {
               ),
               SizedBox(height: 70),
               TextField(
+                focusNode: _typeFocus,
                 controller: typeController,
                 decoration: InputDecoration(
                   hintText: '약물 유형을 작성해주세요.',
                 ),
+                onSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_nameFocus),
               ),
               SizedBox(height: 16),
               TextField(
+                focusNode: _nameFocus,
                 controller: nameController,
                 decoration: InputDecoration(
                   hintText: '약물 이름을 작성해주세요.',
                 ),
+                onSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_doseFocus),
               ),
               SizedBox(height: 16),
               TextField(
+                focusNode: _doseFocus,
                 controller: doseController,
                 decoration: InputDecoration(
                   hintText: '약물 용량을 작성해주세요.',
                 ),
+                onSubmitted: (_) => _saveDrugData(),
               ),
               SizedBox(height: 32),
               SaveButton(
-                typeController: typeController,
-                nameController: nameController,
-                doseController: doseController,
-                dateTimeController: selectedDateTime,
-                isBeforeMeal: isBeforeMeal,
+                onSave: _saveDrugData,
               ),
             ],
           ),
@@ -175,19 +237,9 @@ class _DrugInputPageState extends State<DrugInputPage> {
 }
 
 class SaveButton extends StatelessWidget {
-  final TextEditingController typeController;
-  final TextEditingController nameController;
-  final TextEditingController doseController;
-  final DateTime dateTimeController;
-  final bool isBeforeMeal;
+  final VoidCallback onSave;
 
-  SaveButton({
-    required this.typeController,
-    required this.nameController,
-    required this.doseController,
-    required this.dateTimeController,
-    required this.isBeforeMeal,
-  });
+  SaveButton({required this.onSave});
 
   @override
   Widget build(BuildContext context) {
@@ -195,15 +247,7 @@ class SaveButton extends StatelessWidget {
       width: double.infinity,
       height: 60,
       child: ElevatedButton(
-        onPressed: () {
-          sendDrugDataToServer(
-            typeController.text,
-            nameController.text,
-            doseController.text,
-            dateTimeController,
-            isBeforeMeal,
-          );
-        },
+        onPressed: onSave,
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.dosePrimaryColor,
           shape: RoundedRectangleBorder(
@@ -217,48 +261,5 @@ class SaveButton extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void sendDrugDataToServer(
-    String type,
-    String name,
-    String dose,
-    DateTime dateTime,
-    bool isBeforeMeal,
-  ) async {
-    final token = UserTokenManager.getToken();
-
-    if (token != null) {
-      final data = {
-        'medicine_type': type,
-        'dose_medicine': name,
-        'dose_amount': dose,
-        'dose_time': DateFormat('yyyy-MM-dd HH:mm').format(dateTime),
-        'meal_yn': isBeforeMeal ? 'N' : 'Y',
-      };
-
-      final url = Uri.parse('${dotenv.env['BACKEND_URL']}/dose');
-
-      try {
-        final response = await http.post(
-          url,
-          headers: {
-            'Authorization': 'Bearer $token',
-            'Content-Type': 'application/json',
-          },
-          body: jsonEncode(data),
-        );
-
-        if (response.statusCode == 201) {
-          print('약물 정보가 성공적으로 전송되었습니다.');
-        } else {
-          print('약물 정보 전송에 실패했습니다. 에러 코드: ${response.statusCode}');
-        }
-      } catch (error) {
-        print('약물 저장 오류: $error');
-      }
-    } else {
-      print('토큰 없음');
-    }
   }
 }
